e304bffc4fb7612137a56084c7df5fbd93026cd8
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-08-14 02:20:07 +0800

    auto clone nix and nvim config on home activation

--- flake.nix
+++ flake.nix
@@ -179,7 +179,6 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.madmax = import ./home/madmax-mbp.nix;
-            home-manager.backupFileExtension = "backup";
           }
         ];
       };
