49fc086bcad661fef2f023436a8d185c819a35f6
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-09-01 18:09:02 +0800

    load yabai scripting addition

--- home/maxcchuang.nix
+++ home/maxcchuang.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   home.username = "maxcchuang";
@@ -13,6 +13,13 @@
         identityFile = "${config.home.homeDirectory}/.ssh/id_ed25519";
       };
     };
+  };
+
+  # must sudo explicitly, the sudoer file does not work on this machine
+  home.activation = let yabai = "${pkgs.yabai}/bin/yabai";
+  in {
+    load_yabai_sa = lib.hm.dag.entryAfter [ "writeBoundary" ]
+      "$DRY_RUN_CMD /usr/bin/sudo ${yabai} --load-sa";
   };
 
   imports = [ ./defaults.nix ];
