fce77f15d3115488c27adaf44543a316a8ea1029
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-09-01 17:59:13 +0800

    fix focus window in space script

--- dotfiles/yabai/focus_window_in_space.sh
+++ dotfiles/yabai/focus_window_in_space.sh
@@ -5,15 +5,15 @@ PATH="/run/current-system/sw/bin:$PATH"
 
 space_index=$1
 
-# if any window other than Arc PIP and PastePal has focus
+# if any non sticky window has focus
 space_has_focus=$(yabai -m query --windows --space "$space_index" \
-    | jq '[ .[] | select((.app == "Arc" or .app == "PastePal") and .["is-floating"] | not) ] | map(.["has-focus"]) | any')
+    | jq '[ .[] | select(."is-sticky" | not) ] | map(.["has-focus"]) | any')
 
 if [[ "$space_has_focus" = "false" ]]; then
     first_window_id="null"
-    # the first window in the space that is not Arc PIP
+    # the first window in the space that is not sticky
     first_window_id=$(yabai -m query --windows --space "$space_index" \
-        | jq '[ .[] | select((.app == "Arc" or .app == "PastePal") and .["is-floating"] | not) ].[0].id')
+        | jq '[ .[] | select(."is-sticky" | not) ].[0].id')
 
     if [[ "$first_window_id" != "null" ]]; then
         yabai -m window --focus "$first_window_id"
