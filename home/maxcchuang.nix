8070f93f0b7b30959f7618ce280c0d6f59b15b36
maxcchuang <maxcchuang@google.com>
2024-09-05 16:08:54 +0800

    add zoxide and atuin

--- home/maxcchuang.nix
+++ home/maxcchuang.nix
@@ -30,6 +30,20 @@
 
   home.sessionPath = [ "${config.home.homeDirectory}/.local/bin" ];
 
+  home.sessionVariables.PAGER = "bat -p";
+
+  programs.zoxide.enable = true;
+  home.sessionVariables = {
+    "_ZO_DATA_DIR" = "${config.home.homeDirectory}/.local/share";
+    "_ZO_RESOLVE_SYMLINKS" = "1";
+    "_ZO_EXCLUDE_DIRS" = "/nix/store/*";
+  };
+
+  programs.atuin = {
+    enable = true;
+    flags = [ "--disable-up-arrow" ];
+  };
+
   programs.fish.shellAbbrs = { hm = "home-manager"; };
 
   imports = [ ./modules/fish.nix ./modules/nvim.nix ./modules/tmux.nix ];
