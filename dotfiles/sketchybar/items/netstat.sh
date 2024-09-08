690e5d044cf0b008c2ac5aa248c6e9df7934b922
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-09-08 19:04:41 +0800

    fix netstat script

--- dotfiles/sketchybar/items/netstat.sh
+++ dotfiles/sketchybar/items/netstat.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pkill netstat -w1
-netstat -w1 \
-  | awk 'NR > 3 && NR%3==0 {print "{upload=" $3 ",download=" $6 "}"; fflush(stdout) }' \
+pkill -f "netstat -w5"
+netstat -w5 \
+  | awk 'NR > 2 {print "{download=" $3/5 ", upload=" $6/5 "}"; fflush(stdout) }' \
   | xargs -I {} sketchybar --trigger netstat_update INFO={} &
