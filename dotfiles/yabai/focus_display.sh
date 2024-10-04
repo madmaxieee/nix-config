a806e5c8b85cf70251c234a2c3e3464c9910387c
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-10-05 03:47:23 +0800

    fix move window focus scripts

--- dotfiles/yabai/focus_display.sh
+++ dotfiles/yabai/focus_display.sh
@@ -5,6 +5,6 @@ PATH="/run/current-system/sw/bin:$PATH"
 
 direction=$1
 
-visible_space_index=$(yabai -m query --spaces --display "$direction" | jq '.[] | select(.["is-visible"]==true) | .index')
+visible_space_index=$(yabai -m query --spaces is-visible,index --display "$direction" | jq '.[] | select(.["is-visible"]==true) | .index')
 
 ~/.config/yabai/focus_window_in_space.sh "$visible_space_index" || yabai -m display --focus "$direction"
--- dotfiles/yabai/restore_window_focus.sh
+++ dotfiles/yabai/restore_window_focus.sh
@@ -3,6 +3,6 @@
 
 PATH="/run/current-system/sw/bin:$PATH"
 
-current_space=$(yabai -m query --spaces | jq '[ .[] | select(."has-focus")][0].id')
+current_space=$(yabai -m query --spaces has-focus,index | jq '[ .[] | select(."has-focus")][0].index')
 
 ~/.config/yabai/focus_window_in_space.sh "$current_space" || exit 1
