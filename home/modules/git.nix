f024c7cf236049e2d5192781f79361881ddf7304
maxcchuang <maxcchuang@google.com>
2024-10-30 11:53:55 +0000

    add git aliases for skip worktree

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -10,31 +10,40 @@
 
     aliases = {
       "st" = "status -sb";
+
       "a" = "add";
       "aa" = "add --all";
       "ua" = "restore --staged";
+
       "cm" = "commit -m";
       "cam" = "commit -a -m";
       "amend" = "commit --amend";
+
       "d" = "diff";
       "ds" = "diff --staged";
+
       "f" = "fetch";
       "fa" = "fetch --all";
+
       "rb" = "rebase";
       "cp" = "cherry-pick";
+
       "p" = "push";
       "pushf" = "push --force-with-lease";
+
       "co" = "checkout";
       "sw" = "switch";
       "br" = "branch";
       "brl" = "branch -l";
       "brr" = "branch -r";
       "wt" = "worktree";
+
       "sa" = "stash apply";
       "ss" = "stash push";
       "sp" = "stash pop";
       "sl" =
         "stash list --pretty=format:'%C(red)%h%C(reset) - %C(dim yellow)(%C(bold magenta)%gd%C(dim yellow))%C(reset) %<(70,trunc)%s %C(green)(%cr) %C(bold blue)<%an>%C(reset)'";
+
       "lg" =
         "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
       "llog" =
@@ -43,10 +52,15 @@
         "log --pretty=format:'%Cgreen%h %Creset%cd %Cblue[%cn] %Creset%s%C(yellow)%d%C(reset)' --graph --date=relative --decorate --all";
       "rlog" =
         "reflog --pretty=format:'%Cred%h%Creset %C(auto)%gd%Creset %C(auto)%gs%C(reset) %C(green)(%cr)%C(reset) %C(bold blue)<%an>%Creset' --abbrev-commit";
+
       "sub" = "submodule";
       "zip" = "archive HEAD --output";
       "tgz" = "archive --format=tgz HEAD --output";
       "bs" = "bisect";
+      "ls" = "ls-files";
+
+      "ignore" = "update-index --skip-worktree";
+      "noignore" = "update-index --no-skip-worktree";
     };
 
     ignores = [
