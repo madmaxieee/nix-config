76db8012cfa560ca1d721da6138647afc298ad1f
maxcchuang <maxcchuang@google.com>
2024-11-12 10:26:13 +0000

    add hostname module in the prompt for easier visibility

--- dotfiles/tmux/scripts/sesh.sh
+++ dotfiles/tmux/scripts/sesh.sh
@@ -7,21 +7,24 @@ fi
 tmux new-session -d -s main -c "$HOME" > /dev/null 2>&1 
 
 if which starship &> /dev/null; then
-	border_label=" sesh $(starship module hostname | cut -d' ' -f1) "
+	hostname_module="$(starship module hostname | cut -d' ' -f1) "
 else
-	border_label=' sesh '
+	hostname_module=''
 fi
+
+border_label=" sesh $hostname_module"
+prompt_prefix="$hostname_module"
 
 session="$(
  sesh list | grep -v -E '.__popup$' | fzf-tmux -p 55%,60% \
-		--no-sort --border-label "$border_label" --prompt '⚡  ' \
-		--header '  ^a all ^t tmux ^g configs ^x zoxide ^d tmux kill ^f find' \
+		--no-sort --border-label "$border_label" --prompt "$prompt_prefix"'⚡  ' \
+		--header '  ^a all ^t tmux ^g configs ^x zoxide ^d kill ^f find' \
 		--bind 'tab:down,btab:up' \
-		--bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list)' \
-		--bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t)' \
-		--bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c)' \
-		--bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z)' \
-		--bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash -E .cache . ~)' \
+		--bind 'ctrl-a:change-prompt('"$prompt_prefix"'⚡  )+reload(sesh list)' \
+		--bind 'ctrl-t:change-prompt('"$prompt_prefix"'🪟  )+reload(sesh list -t)' \
+		--bind 'ctrl-g:change-prompt('"$prompt_prefix"'⚙️  )+reload(sesh list -c)' \
+		--bind 'ctrl-x:change-prompt('"$prompt_prefix"'📁  )+reload(sesh list -z)' \
+		--bind 'ctrl-f:change-prompt('"$prompt_prefix"'🔎  )+reload(fd -H -d 2 -t d -E .Trash -E .cache . ~)' \
 		--bind 'ctrl-d:execute(tmux kill-session -t {})+change-prompt(⚡  )+reload(sesh list)' \
 		--bind 'ctrl-alt-k:abort'
 )"
