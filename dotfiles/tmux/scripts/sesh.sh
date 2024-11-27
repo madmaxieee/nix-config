ff5aff8d242b04d3a0e1640353e995ccb8a1a776
maxcchuang <maxcchuang@google.com>
2024-11-27 19:11:21 +0800

    qol improvements on obsidian session

--- dotfiles/tmux/scripts/sesh.sh
+++ dotfiles/tmux/scripts/sesh.sh
@@ -1,32 +1,32 @@
 #! /usr/bin/env bash
 
 if [[ "$(tmux show-options detach-on-destroy)" == "detach-on-destroy on" ]]; then
-	exit 0
+  exit 0
 fi
 
 tmux new-session -d -s main -c "$HOME" > /dev/null 2>&1 
 
 if which starship &> /dev/null; then
-	hostname_module="$(starship module hostname | cut -d' ' -f1) "
+  hostname_module="$(starship module hostname | cut -d' ' -f1) "
 else
-	hostname_module=''
+  hostname_module=''
 fi
 
 border_label=" sesh $hostname_module"
 prompt_prefix="$hostname_module"
 
 session="$(
- sesh list | ~/.config/tmux/scripts/remove_duplicate_sessions.py | grep -v -E '.__popup$' | fzf-tmux -p 55%,60% \
-		--no-sort --border-label "$border_label" --prompt "$prompt_prefix"'⚡  ' \
-		--header '  ^a all ^t tmux ^g configs ^x zoxide ^d kill ^f find' \
-		--bind 'tab:down,btab:up' \
-		--bind 'ctrl-a:change-prompt('"$prompt_prefix"'⚡  )+reload(sesh list | ~/.config/tmux/scripts/remove_duplicate_sessions.py)' \
-		--bind 'ctrl-t:change-prompt('"$prompt_prefix"'🪟  )+reload(sesh list -t)' \
-		--bind 'ctrl-g:change-prompt('"$prompt_prefix"'⚙️  )+reload(sesh list -c)' \
-		--bind 'ctrl-x:change-prompt('"$prompt_prefix"'📁  )+reload(sesh list -z)' \
-		--bind 'ctrl-f:change-prompt('"$prompt_prefix"'🔎  )+reload(fd -H -d 2 -t d -E .Trash -E .cache . ~)' \
-		--bind 'ctrl-d:execute(tmux kill-session -t {})+change-prompt(⚡  )+reload(sesh list)' \
-		--bind 'ctrl-alt-k:abort'
+  sesh list | ~/.config/tmux/scripts/remove_duplicate_sessions.py | grep -v -E '.__popup$' | fzf-tmux -p 55%,60% \
+    --no-sort --border-label "$border_label" --prompt "$prompt_prefix"'⚡  ' \
+    --header '  ^a all ^t tmux ^g configs ^x zoxide ^d kill ^f find' \
+    --bind 'tab:down,btab:up' \
+    --bind 'ctrl-a:change-prompt('"$prompt_prefix"'⚡  )+reload(sesh list | ~/.config/tmux/scripts/remove_duplicate_sessions.py)' \
+    --bind 'ctrl-t:change-prompt('"$prompt_prefix"'🪟  )+reload(sesh list -t)' \
+    --bind 'ctrl-g:change-prompt('"$prompt_prefix"'⚙️  )+reload(sesh list -c)' \
+    --bind 'ctrl-x:change-prompt('"$prompt_prefix"'📁  )+reload(sesh list -z)' \
+    --bind 'ctrl-f:change-prompt('"$prompt_prefix"'🔎  )+reload(fd -H -d 2 -t d -E .Trash -E .cache . ~)' \
+    --bind 'ctrl-d:execute(tmux kill-session -t {})+change-prompt(⚡  )+reload(sesh list)' \
+    --bind 'ctrl-alt-k:abort'
 )"
 
 tmux set-option -t "$session" detach-on-destroy off &
