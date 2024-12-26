58e218ce4fdc8239502a8b961283d4421777a2e9
Max Chuang <maxcchuang@google.com>
2024-12-26 13:37:40 +0800

    use flash as default model for mods

--- home/modules/mods/gemini.nix
+++ home/modules/mods/gemini.nix
@@ -2,7 +2,7 @@
   programs.fish.functions.mods = {
     body = ''
       set -x GOOGLE_API_KEY (pass gemini/mods 2> /dev/null)
-      command mods --model gemini $argv
+      command mods --model flash --topk 40 $argv
     '';
     wraps = "mods";
   };
