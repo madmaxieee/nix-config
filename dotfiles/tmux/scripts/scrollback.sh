1965fc496c33c3241e8eb605fb73ab8874d900c7
Max Chuang <maxcchuang@google.com>
2024-12-31 00:18:29 +0800

    refactor scrollback.sh

--- dotfiles/tmux/scripts/scrollback.sh
+++ dotfiles/tmux/scripts/scrollback.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
 
-file=$(mktemp).sh
-echo "#shellcheck disable=all" > "$file"
+file=$(mktemp)
 tmux capture-pane -pS -32768 >> "$file"
 tmux split-window bash -c "TMUX_SCROLLBACK=1 nvim '+ normal G' $file"
 tmux resize-pane -Z
