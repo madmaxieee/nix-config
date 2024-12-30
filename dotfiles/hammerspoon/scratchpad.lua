3b53472ce8d1568823ebda1bc7871edf4a59c60e
Max Chuang <maxcchuang@google.com>
2024-12-31 00:27:32 +0800

    configure nvim obsidian

--- dotfiles/hammerspoon/scratchpad.lua
+++ dotfiles/hammerspoon/scratchpad.lua
@@ -2,26 +2,58 @@ local M = {}
 
 hs.window.animationDuration = 0
 
-M.toggle_scratchpad = function(app_name)
+---@class ScratchpadOpts
+---@field window_title string?
+---@field launch_fn function?
+
+---@param app_name string: the name of the application to toggle
+---@param opts ScratchpadOpts?
+M.toggle_scratchpad = function(app_name, opts)
+    opts = opts or {}
+
+    local launch = function()
+        if opts.launch_fn then
+            opts.launch_fn()
+        else
+            hs.application.launchOrFocus(app_name)
+        end
+    end
+
+    local find_window = function(app)
+        if not opts.window_title then
+            return app:mainWindow(), app
+        end
+        local window = hs.appfinder.windowFromWindowTitle(opts.window_title)
+        if window == nil then
+            return nil, nil
+        end
+        app = window:application()
+        if app:name() == app_name then
+            return window, app
+        end
+    end
+
+    ---@type hs.application?
     local app = hs.application.find(app_name, true)
     if not app then
-        hs.application.launchOrFocus(app_name)
+        launch()
         return
     end
 
-    local main_window = app:mainWindow()
-    if not main_window then
-        hs.application.launchOrFocus(app_name)
+    local window = nil
+    window, app = find_window(app)
+    if not window or not app then
+        launch()
         return
     end
 
-    if app:isFrontmost() then
+    if window:id() == hs.window.frontmostWindow():id() then
         app:hide()
     else
-        hs.execute([[PATH=]] .. PATH .. [[ ~/.config/yabai/move_window_to_current_space.sh ]] .. main_window:id())
-        hs.spaces.moveWindowToSpace(main_window, hs.spaces.activeSpaceOnScreen())
-        main_window:moveToScreen(hs.screen.mainScreen())
-        app:activate()
+        hs.execute([[PATH=]] .. PATH .. [[ ~/.config/yabai/move_window_to_current_space.sh ]] .. window:id())
+        hs.spaces.moveWindowToSpace(window, hs.spaces.activeSpaceOnScreen())
+        window:moveToScreen(hs.screen.mainScreen())
+        window:focus():raise()
     end
 end
 
