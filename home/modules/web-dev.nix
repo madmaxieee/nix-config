465d2fc288ea2866f5649a0a05ba12e3362bdc27
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2025-01-25 13:40:50 +0800

    add bun abbr

--- home/modules/web-dev.nix
+++ home/modules/web-dev.nix
@@ -2,17 +2,15 @@
   home.packages = with pkgs; [ fnm deno ];
 
   programs.fish.shellAbbrs = {
-    d = "docker";
-    dco = "docker compose";
-    yy = "yarn";
+    b = "bun";
     pm = "pnpm";
+    yy = "yarn";
   };
 
   programs.zsh.zsh-abbr.abbreviations = {
-    d = "docker";
-    dco = "docker compose";
-    yy = "yarn";
+    b = "bun";
     pm = "pnpm";
+    yy = "yarn";
   };
 
   programs.bun.enable = true;
