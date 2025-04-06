eba35c5a1c5176deda72ae0773bdec8b8cddd4ed
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2025-04-06 11:27:59 +0800

    update catpuccin tmux config

--- home/modules/tmux.nix
+++ home/modules/tmux.nix
@@ -17,12 +17,17 @@ in {
       {
         plugin = tmuxPlugins.catppuccin;
         extraConfig = ''
+          set -g status-left-length 100
+          set -g status-right-length 100
+
+          set -g status-left " #{E:@catppuccin_status_session}"
+          set -g status-right "#{E:@catppuccin_status_application}"
+          set -ag status-right "#{E:@catppuccin_status_session}"
+
           set -g @catppuccin_flavour 'mocha'
           set -g @catppuccin_window_status_style 'rounded'
-          set -g status-left-length 100
-          set -g status-left "#{E:@catppuccin_status_session}"
           set -g @catppuccin_window_current_text " #{pane_current_path}#{?window_zoomed_flag, ,}"
-          set -g @catppuccin_status_right_separator '█ '
+          set -g @catppuccin_status_right_separator ' '
           set -g @catppuccin_status_connect_separator 'no'
         '';
       }
