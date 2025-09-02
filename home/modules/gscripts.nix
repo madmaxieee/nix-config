b5450bf46b9181e591fce3f7304ada2f2db7163b
Max Chuang <maxcchuang@google.com>
2025-09-02 15:27:58 +0800

    add extract-util gscript

--- home/modules/gscripts.nix
+++ home/modules/gscripts.nix
@@ -6,12 +6,14 @@ let
     config.lib.file.mkOutOfStoreSymlink "${nix_config_path}/dotfiles/${path}";
 in {
   home.sessionPath = [ "${config.home.homeDirectory}/.local/bin" ];
-  home.file = if platform == "glinux" then {
+  home.file = {
+    ".local/bin/extract-util".source = linkDotfile "gscripts/extract-util";
+  } // (if platform == "glinux" then {
     ".local/bin/gob-create".source = linkDotfile "gscripts/gob-create";
     ".local/bin/gerrit-init".source = linkDotfile "gscripts/gerrit-init";
     ".local/bin/x20-share".source = linkDotfile "gscripts/x20-share";
   } else if platform == "gmac" then {
     ".local/bin/fetch-artifact".source = linkDotfile "gscripts/fetch-artifact";
   } else
-    (throw ("gscripts: platform must be one of glinux or gmac"));
+    (throw ("gscripts: platform must be one of glinux or gmac")));
 }
