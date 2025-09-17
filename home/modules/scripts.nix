901bb1b9e1bcdb1390ddcb06b32080f7f4c1e6cf
Max Chuang <maxcchuang@google.com>
2025-09-17 20:13:10 +0800

    add git-foreach

--- home/modules/scripts.nix
+++ home/modules/scripts.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   nix_config_path = "${config.home.homeDirectory}/nix-config";
@@ -6,10 +6,17 @@ let
     config.lib.file.mkOutOfStoreSymlink "${nix_config_path}/dotfiles/${path}";
 in {
   home.sessionPath = [ "${config.home.homeDirectory}/.local/bin" ];
-  home.file = {
-    ".local/bin/nr".source = linkDotfile "scripts/nr";
-    ".local/bin/ns".source = linkDotfile "scripts/ns";
-    ".local/bin/things".source = linkDotfile "scripts/things";
-    ".local/bin/vipe".source = linkDotfile "scripts/vipe";
-  };
+
+  home.file = lib.mkMerge [
+    {
+      ".local/bin/git-foreach".source = linkDotfile "scripts/git-foreach";
+      ".local/bin/nr".source = linkDotfile "scripts/nr";
+      ".local/bin/ns".source = linkDotfile "scripts/ns";
+      ".local/bin/vipe".source = linkDotfile "scripts/vipe";
+    }
+
+    (lib.mkIf pkgs.stdenv.isDarwin {
+      ".local/bin/things".source = linkDotfile "scripts/things";
+    })
+  ];
 }
