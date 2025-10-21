7d1e6831555851b678526f577c835f905d5a1a2f
Max Chuang <maxcchuang@google.com>
2025-10-21 16:49:10 +0800

    fix: Improve Hammerspoon PATH construction
    Refactor PATH construction in Hammerspoon to explicitly include /opt/homebrew/bin and
    handle cases where NIX_PATH might be nil, ensuring more robust path resolution.

--- dotfiles/hammerspoon/init.lua
+++ dotfiles/hammerspoon/init.lua
@@ -4,8 +4,13 @@ require "hs.ipc"
 require "reload"
 
 pcall(require, "nix_path")
-NIX_PATH = NIX_PATH or ""
-PATH = NIX_PATH .. ":" .. os.getenv "PATH"
+
+NIX_PATH = NIX_PATH or nil
+if NIX_PATH then
+    PATH = table.concat({ NIX_PATH, "/opt/homebrew/bin", os.getenv "PATH" }, ":")
+else
+    PATH = table.concat({ "/opt/homebrew/bin", os.getenv "PATH" }, ":")
+end
 
 hs.application.enableSpotlightForNameSearches(true)
 
