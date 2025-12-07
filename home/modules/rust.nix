b4d34feae51683126ded5f9012b3d96ff214b79d
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2025-12-07 22:41:58 +0800

    feat(rust): Enable continuous test runner 'bacon'
    Enables the `programs.bacon` Home Manager module, integrating a
    continuous test and documentation generator tool for Rust development
    workflows.

--- home/modules/rust.nix
+++ home/modules/rust.nix
@@ -2,6 +2,8 @@
   home.packages = with pkgs; [ rustup ];
   home.sessionPath = [ "${config.home.homeDirectory}/.cargo/bin" ];
 
+  programs.bacon.enable = true;
+
   home.activation = let rustup = "${pkgs.rustup}/bin/rustup";
   in {
     rustup_setup = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
