a2f8ec79ae25e486e38b62375961fa0545ed9fb2
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2025-12-07 22:49:48 +0800

    chore(ssh): Disable default Home Manager SSH config
    Disables the automatic default configuration provided by Home Manager
    for `programs.ssh` by setting `enableDefaultConfig = false`. This
    change ensures all SSH client settings are managed explicitly within
    the configuration files.

--- home/madmax-mbp.nix
+++ home/madmax-mbp.nix
@@ -6,8 +6,23 @@
 
   home.packages = with pkgs; [ zig typst ];
 
+  # former default options
+  programs.ssh.matchBlocks."*" = {
+    forwardAgent = false;
+    addKeysToAgent = "no";
+    compression = false;
+    serverAliveInterval = 0;
+    serverAliveCountMax = 3;
+    hashKnownHosts = false;
+    userKnownHostsFile = "~/.ssh/known_hosts";
+    controlMaster = "no";
+    controlPath = "~/.ssh/master-%r@%n:%p";
+    controlPersist = "no";
+  };
+
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
     matchBlocks = {
       "linode" = {
         hostname = "linode.madmaxieee.dev";
