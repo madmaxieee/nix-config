3ec368ccc52d5cb9709df55479e7427c4b38dc4f
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2026-02-25 12:52:42 +0000

    chore: Add jujutsu aliases and shell abbreviations
    Adds common aliases to the Jujutsu configuration, including e for edit,
    rb for rebase, and llog for detailed logging.
    
    Also defines 'j' as a shell abbreviation for 'jj' in both Fish and Zsh
    to improve terminal workflow.

--- home/modules/jujutsu.nix
+++ home/modules/jujutsu.nix
@@ -3,10 +3,19 @@
 let
   linkDotfile = config.lib.custom.linkDotfile;
 in
-{
+rec {
   home.packages = with pkgs; [
     jujutsu
     delta
   ];
+
   xdg.configFile."jj".source = linkDotfile "jujutsu";
+
+  programs.fish.shellAbbrs = {
+    j = "jj";
+  };
+
+  programs.zsh = {
+    zsh-abbr.abbreviations = programs.fish.shellAbbrs;
+  };
 }
