20b5b7add674087bb4be649d92d64f4d43950bba
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2026-03-04 00:56:34 +0800

    chore: Split jj config.toml and conf.d
    Update the jujutsu module to link jj/config.toml and jj/conf.d
    explicitly instead of symlinking the entire jj config directory.

--- home/modules/jujutsu.nix
+++ home/modules/jujutsu.nix
@@ -14,7 +14,10 @@ rec {
     ]
     ++ pkgs.lib.optional (!use_system_binary) jujutsu;
 
-  xdg.configFile."jj".source = linkDotfile "jujutsu";
+  xdg.configFile = {
+    "jj/config.toml".source = linkDotfile "jujutsu/config.toml";
+    "jj/conf.d".source = linkDotfile "jujutsu/conf.d";
+  };
 
   programs.fish.shellAbbrs = {
     j = "jj";
