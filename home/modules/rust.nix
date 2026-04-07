00a802742e5e1a5726c296719effee1c08bd52b7
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2026-04-07 23:44:20 +0800

    chore: Configure Cargo to use git CLI
    Adds a Cargo config file at `~/.cargo/config.toml` to enable
    `git-fetch-with-cli`. This makes Cargo use the system git client for
    fetching dependencies.

--- home/modules/rust.nix
+++ home/modules/rust.nix
@@ -2,6 +2,13 @@
 {
   home.sessionPath = [ "${config.home.homeDirectory}/.cargo/bin" ];
 
+  home.file = {
+    ".cargo/config.toml".text = ''
+      [net]
+      git-fetch-with-cli = true
+    '';
+  };
+
   programs.bacon.enable = true;
 
   programs.mise = {
