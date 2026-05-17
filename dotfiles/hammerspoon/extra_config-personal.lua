185886f41065c76743ac5be4fbdf53ca84a86cbf
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2026-05-17 23:18:51 +0800

    feat(hammerspoon): update default browser to Helium
    Change the browser configuration from Zen to Helium in personal
    Hammerspoon extra configuration.

--- dotfiles/hammerspoon/extra_config-personal.lua
+++ dotfiles/hammerspoon/extra_config-personal.lua
@@ -1,6 +1,6 @@
 return {
     ai_app = "T3 Chat",
-    browser = "Zen",
+    browser = "Helium",
     message_app = "Messenger",
     terminal_app = "Ghostty",
     todo_app = "com.culturedcode.ThingsMac",
